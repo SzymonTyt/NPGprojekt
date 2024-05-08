8a84fe8e5895ba45bb245ef8bac96f8f45f95c3f
Ignacy Zdziech <164023987+xIgnacY20x@users.noreply.github.com>
2024-05-08 15:07:54 +0200

    Zmieniona treść i nazwy
    Po odkryciu biblioteki customtkinter, postanowiłem zastosować ją, zamiast klasycznie dostępnej tkinter, ponieważ pozwala ona na logiczniejsze tworzenie nowoczesnego okienka (np jak w naszym przypadku menu gry). Plik zawiera podstawowy szkielet menu głównego, z funkcją zamykania go. Dodatkowe zmieniłem nazwę pliku na menu.py

---
+++ menu.py
@@ -0,0 +1,26 @@
+from customtkinter import CTk, CTkButton, CTkSlider, CTkFrame, set_appearance_mode, CTkComboBox, CTkToplevel, CTkLabel
+
+def start_game():
+    print("Game started!")
+
+def quit_game():
+    app.destroy()
+  
+app = CTk()
+app.title("Menu Gry")
+app.geometry("500x400")
+set_appearance_mode("dark")
+
+frame = CTkFrame(app)
+frame.pack(pady=60)
+
+start_button = CTkButton(frame, text="Start", corner_radius=32, fg_color="#4158D0", hover_color="#C850C0", command=start_game)
+start_button.grid(row=0, column=0, padx=50, pady=30)
+
+settings_button = CTkButton(frame, text="Ustawienia", corner_radius=32, fg_color="#4158D0", hover_color="#C850C0")
+settings_button.grid(row=1, column=0, padx=50, pady=30)
+
+quit_button = CTkButton(frame, text="Wyjście", corner_radius=32, fg_color="#4158D0", hover_color="#C850C0", command=quit_game)
+quit_button.grid(row=2, column=0, padx=50, pady=30)
+
+app.mainloop()
