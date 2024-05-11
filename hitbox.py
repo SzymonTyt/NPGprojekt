e772d30bf66f9d019be01c36653d954e36a2bf36
Adam Frydel <adas.frydel@gmail.com>
2024-05-11 12:59:35 +0200

    Doszliwowanie kodu na poruszanie hitboxem

--- hitbox.py
+++ hitbox.py
@@ -9,13 +9,13 @@ def hitbox_ustalony_przez_gracza(x, y, szerokosc, wysokosc):
     return hitbox
 
 def main():
+
+    szerokoscHb, wysokoscHb = map(int, input("Jaka wielkosc ma miec hitbox postaci (szerokosc wysokosc): ").split())
     window = pygame.display.set_mode((800, 500))  # Ustawienie rozmiaru okna
     pygame.display.set_caption('Magiczne przygody kamienia Artura')  # Etykieta okna
 
-    szerokoscHb, wysokoscHb = map(int, input("Jaka wielkosc ma miec hitbox postaci (szerokosc wysokosc): ").split())
-
     hitbox = hitbox_ustalony_przez_gracza(0, 0, szerokoscHb, wysokoscHb)
-    hitbox_speed = 5  # Szybkosc poruszania hitboxa
+    hitbox_speed = 1  # Szybkosc poruszania hitboxa
 
     while True:
         for event in pygame.event.get():
@@ -27,12 +27,16 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[K_a]:
             hitbox.x -= hitbox_speed
+            pygame.time.wait(5)
         if keys[K_d]:
             hitbox.x += hitbox_speed
+            pygame.time.wait(5)
         if keys[K_w]:
             hitbox.y -= hitbox_speed
+            pygame.time.wait(5)
         if keys[K_s]:
             hitbox.y += hitbox_speed
+            pygame.time.wait(5)
 
         # Sprawdzenie, czy hitbox dotyka krawedzi okna
         if hitbox.left <= 0:
@@ -44,8 +48,9 @@ def main():
         if hitbox.bottom >= 500:
             hitbox.bottom = 500
 
-        window.fill((255, 255, 255))  # Wypelnienie okna na bialo
-        pygame.draw.rect(window, (0, 0, 0), hitbox)  # Rysowanie hitboxa na ekranie
+        window.fill("purple")
+
+        pygame.draw.rect(window, "red", hitbox)  # Rysowanie hitboxa na ekranie
 
         pygame.display.update()
 
