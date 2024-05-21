65e8dd1ca39c91597fdd5f1ac9dfc169e1cb1464
Adam Frydel <adas.frydel@gmail.com>
2024-05-22 00:21:41 +0200

    Dodanie odbicia postaci w momencie zderzenia

--- hitbox + pasekZdrowia/reakcja na kolizje.py
+++ hitbox + pasekZdrowia/reakcja na kolizje.py
@@ -8,3 +8,19 @@ def czy_kolizja(hitbox, inny_obiekt ):
              return True
     return False
 
+def odbicie(hitbox, inny_obiekt):
+    # Jeśli występuje kolizja, oblicz kierunek odbicia
+    if czy_kolizja(hitbox, inny_obiekt):
+        # Oblicz różnicę położenia między hitboxami
+        dx = (hitbox['x'] + hitbox['szerokość']/2) - (inny_obiekt['x'] + inny_obiekt['szerokość']/2)
+        dy = (hitbox['y'] + hitbox['wysokość']/2) - (inny_obiekt['y'] + inny_obiekt['wysokość']/2)
+
+        # Jeśli wartość bezwzględna dx jest większa niż wartość bezwzględna dy,
+        # odbicie będzie w kierunku poziomym
+        if abs(dx) > abs(dy):
+            hitbox['predkosc_x'] *= -1
+            inny_obiekt['predkosc_x'] *= -1
+        # W przeciwnym razie odbicie będzie w kierunku pionowym
+        else:
+            hitbox['predkosc_y'] *= -1
+            inny_obiekt['predkosc_y'] *= -1
