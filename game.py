7cef179ef456bc0e66e53d36777e131d9dca7a9a
Ignacy Zdziech <164023987+xIgnacY20x@users.noreply.github.com>
2024-05-21 12:24:10 +0200

    Aktualizacja game.py
    Dodano pasek zdrowia , pozostałe animację oraz zmieniono rozdzielczość okna gry na pełen ekran

--- game.py
+++ game.py
@@ -3,11 +3,12 @@ import pygame
 import random
 
 def start_game(): #funkcja po naciśnięcia przycisku "start"
+
     pygame.init()
 
     height = 720
     width = 1280
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((width, height),pygame.FULLSCREEN)
 
     # informacje o mieczu
     sword_x_size = 70
@@ -15,6 +16,8 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
 
     sword = pygame.image.load("miecz.jpg")
     sword = pygame.transform.scale(sword, (sword_x_size, sword_y_size))
+
+    sword_left = pygame.transform.flip(sword, True, False)
 
     # animacja miecza
     sword_sprite_1 = []
@@ -31,14 +34,14 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
     stone_x_size = 81
     stone_y_size = 54
 
-    stone = pygame.image.load('kamien.png')
+    stone = pygame.image.load('animations/player_spawn/1.png')
     stone = pygame.transform.scale(stone, (stone_x_size, stone_y_size))
 
     # informacje o chrząszczu
     bug_x_size = 75
     bug_y_size = 50
 
-    bug = pygame.image.load('chrzaszcz.png')
+    bug = pygame.image.load('animations/ant_move/1.png')
     bug = pygame.transform.scale(bug, (bug_x_size, bug_y_size))
 
     # losowa początkowa pozycja chrząszcza
@@ -80,6 +83,27 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
     movement_right = True
     movement_down = True
 
+    left = False
+    right = False
+    up = False
+    down = False
+
+    last_left = False
+    last_right = False
+
+    bug_left = False
+    bug_right = False
+    bug_up = False
+    bug_down = False
+
+    mouse = False
+
+    # ruch robaka
+    bug_movement = True
+
+    # start gry
+    game_starting = True
+
     # funkcja logiczna - sprawdzenie czy następuje atak mieczem
     one_hit_stone = False
     one_hit_bug = False
@@ -88,7 +112,63 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
     stone_hp = 100
     bug_hp = 100
 
+    # animacja player_move
+    player_move = []
+    for i in range(1, 10):
+        player_move.append(pygame.transform.scale(pygame.image.load("animations/player_move/" + str(i) + ".png"),
+                                                  (1.5 * stone_x_size, 1.5 * stone_y_size)))
+
+    # animacja ant_move
+    ant_move = []
+    for i in range(1, 7):
+        ant_move.append(pygame.transform.scale(pygame.image.load("animations/ant_move/" + str(i) + ".png"),
+                                               (1.5 * bug_x_size, 1.5 * bug_y_size)))
+
+    # animacja player_spawn
+    player_spawn = []
+    for i in range(1, 32):
+        player_spawn.append(pygame.transform.scale(pygame.image.load("animations/player_spawn/" + str(i) + ".png"),
+                                                   (1.5 * stone_x_size, 1.5 * stone_y_size)))
+
+    # iteratory
+    iter_pm = 1
+    iter_am = 1
+    iter_ps = 1
+
+    class Wrog:
+        def __init__(self, nazwa_wroga, kolor_wroga, wspolrzedna_x, wspolrzedna_y, szerokosc_wroga, wysokosc_wroga,
+                     predkosc):
+            self.nazwa = nazwa_wroga
+            self.kolor = kolor_wroga
+            self.rect_1 = pygame.Rect(0, 0, szerokosc_wroga, wysokosc_wroga)
+            self.rect = pygame.Rect(wspolrzedna_x, wspolrzedna_y, szerokosc_wroga, wysokosc_wroga)
+            self.predkosc = predkosc
+
+        def draw(self, surface):
+            pygame.draw.rect(surface, self.kolor, self.rect)
+
+        def draw_1(self, surface):
+            pygame.draw.rect(surface, self.kolor, self.rect_1)
+
+        def move_towards(self, target_rect):
+            if abs(self.rect.x - target_rect.x) < 5 or abs(self.rect.x - target_rect.y) < 5:
+                if self.rect.x < target_rect.x:
+                    self.rect.x += self.predkosc
+                elif self.rect.x > target_rect.x:
+                    self.rect.x -= self.predkosc
+                if self.rect.y < target_rect.y:
+                    self.rect.y += self.predkosc * 0.6
+                elif self.rect.y > target_rect.y:
+                    self.rect.y -= self.predkosc * 0.6
+
     while program_running:
+        if game_starting:
+            if iter_ps < len(player_spawn):
+                stone = player_spawn[iter_ps]
+                iter_ps += 1
+            else:
+                game_starting = False
+
         clock.tick(60)
 
         # wyłączanie programu
@@ -104,15 +184,32 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
                     hitting = True
                     one_hit_stone = True
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                hitting = True
+                one_hit_stone = True
+                mouse = True
+
         # wciśnięcie klawisza
         key_pressed = pygame.key.get_pressed()
 
         # prostokąty ustalające hitboxy danego obiektu (pisane ręcznie zamiast wykorzystania metody get_rect())
-        rect_sword = pygame.Rect(player_pos.x + stone_x_size, player_pos.y - sword_y_size + (4 * stone_y_size / 5),
-                                 1.1 * sword_x_size, 1.1 * sword_y_size)
-        rect_stone = pygame.Rect(player_pos.x, player_pos.y + 0.1 * stone_y_size, 1.1 * stone_x_size, stone_y_size)
-        rect_bug = pygame.Rect(bug_pos.x + 0.1 * bug_x_size, bug_pos.y + 0.2 * bug_y_size, 0.8 * bug_x_size,
+        if not last_left:
+            rect_sword = pygame.Rect(player_pos.x + stone_x_size, player_pos.y - sword_y_size + (4 * stone_y_size / 5),
+                                     1.1 * sword_x_size, 1.1 * sword_y_size)
+        else:
+            rect_sword = pygame.Rect(player_pos.x - stone_x_size + 30,
+                                     player_pos.y - sword_y_size + (4 * stone_y_size / 5), 1.1 * sword_x_size,
+                                     1.1 * sword_y_size)
+
+        rect_stone = pygame.Rect(player_pos.x + 0.3 * stone_x_size, player_pos.y + 0.5 * stone_y_size,
+                                 0.8 * stone_x_size, 0.9 * stone_y_size)
+        rect_bug = pygame.Rect(bug_pos.x + 0.45 * bug_x_size, bug_pos.y + 0.6 * bug_y_size, 0.7 * bug_x_size,
                                0.8 * bug_y_size)
+        # kwadrat = Wrog("kwadrat", (255, 255, 255), bug_x_rand, bug_y_rand, bug_x_size, bug_y_size, 100)
+
+        # paski zdrowia
+        rect_stone_hp = pygame.Rect(player_pos.x, player_pos.y - 20, stone_hp, 10)
+        rect_bug_hp = pygame.Rect(bug_pos.x, bug_pos.y - 10, bug_hp, 10)
 
         # zablokowanie ruchu, gdy bohater znajdzie się na brzegu ekranu
         if player_pos.x <= 0:
@@ -131,26 +228,135 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
             movement_down = True
 
         # ruch postaci (strzałkami lub WASD)
-        if (key_pressed[pygame.K_w] or key_pressed[pygame.K_UP]) and movement_up:
-            player_pos.y -= 400 * dt
-        if (key_pressed[pygame.K_s] or key_pressed[pygame.K_DOWN]) and movement_down:
-            player_pos.y += 400 * dt
-        if (key_pressed[pygame.K_a] or key_pressed[pygame.K_LEFT]) and movement_left:
-            player_pos.x -= 400 * dt
-        if (key_pressed[pygame.K_d] or key_pressed[pygame.K_RIGHT]) and movement_right:
-            player_pos.x += 400 * dt
+        if not game_starting:
+            if (key_pressed[pygame.K_w] or key_pressed[pygame.K_UP]) and movement_up:
+                player_pos.y -= 400 * dt
+                up = True
+                down = False
+                stone = player_move[iter_pm]
+            elif (key_pressed[pygame.K_s] or key_pressed[pygame.K_DOWN]) and movement_down:
+                player_pos.y += 400 * dt
+                down = True
+                up = False
+                stone = player_move[iter_pm]
+            elif (key_pressed[pygame.K_a] or key_pressed[pygame.K_LEFT]) and movement_left:
+                player_pos.x -= 400 * dt
+                left = True
+                last_left = True
+                right = False
+                last_right = False
+                stone = player_move[iter_pm]
+            elif (key_pressed[pygame.K_d] or key_pressed[pygame.K_RIGHT]) and movement_right:
+                player_pos.x += 400 * dt
+                right = True
+                last_right = True
+                left = False
+                last_left = False
+                stone = player_move[iter_pm]
+            else:
+                right = False
+                left = False
+                up = False
+                down = False
+
+        if mouse:
+            x, y = pygame.mouse.get_pos()
+
+            if x < player_pos.x:
+                left = True
+                last_left = True
+                right = False
+                last_right = False
+            elif x > player_pos.x:
+                left = False
+                last_left = False
+                right = True
+                last_right = True
+            else:
+                left = False
+                last_left = False
+                right = False
+                last_right = False
 
         # ruch przeciwnika
-        if bug_pos.x != 0:
-            if bug_pos.x > player_pos.x:
-                bug_pos.x -= 200 * dt
-            elif bug_pos.x < player_pos.x:
-                bug_pos.x += 200 * dt
-        if bug_pos.y != 0:
-            if bug_pos.y > player_pos.y:
-                bug_pos.y -= 200 * dt
-            if bug_pos.y < player_pos.y:
-                bug_pos.y += 200 * dt
+        if not game_starting:
+            if abs(bug_pos.x - player_pos.x) > 100 * dt:
+                bug_movement = True
+                if bug_pos.x - player_pos.x > 100 * dt:
+                    bug_pos.x -= 200 * dt
+                    bug_left = True
+                    bug_right = False
+                elif bug_pos.x - player_pos.x < -100 * dt:
+                    bug_pos.x += 200 * dt
+                    bug_left = False
+                    bug_right = True
+            else:
+                bug_movement = False
+                bug_left = False
+                bug_right = False
+                bug_up = False
+                bug_down = False
+
+            if abs(bug_pos.y - player_pos.y) > 100 * dt:
+                bug_movement = True
+                if bug_pos.y - player_pos.y > 100 * dt:
+                    bug_pos.y -= 200 * dt
+                    bug_up = True
+                    bug_down = False
+                if bug_pos.y - player_pos.y < -100 * dt:
+                    bug_pos.y += 200 * dt
+                    bug_up = False
+                    bug_down = True
+            else:
+                bug_movement = False
+                bug_left = False
+                bug_right = False
+                bug_up = False
+                bug_down = False
+        else:
+            if bug_pos.x != player_pos.x:
+                bug_movement = True
+                if bug_pos.x > player_pos.x:
+                    bug_pos.x -= 200 * dt
+                    bug_left = True
+                    bug_right = False
+                elif bug_pos.x < player_pos.x:
+                    bug_pos.x += 200 * dt
+                    bug_left = False
+                    bug_right = True
+            else:
+                bug_movement = False
+                bug_left = False
+                bug_right = False
+                bug_up = False
+                bug_down = False
+
+            if bug_pos.y != player_pos.y:
+                bug_movement = True
+                if bug_pos.y > player_pos.y:
+                    bug_pos.y -= 200 * dt
+                    bug_up = True
+                    bug_down = False
+                if bug_pos.y < player_pos.y:
+                    bug_pos.y += 200 * dt
+                    bug_up = False
+                    bug_down = True
+            else:
+                bug_movement = False
+                bug_left = False
+                bug_right = False
+                bug_up = False
+                bug_down = False
+
+        if bug_movement:
+            bug = ant_move[iter_am]
+            iter_am += 1
+
+        if iter_am >= len(ant_move):
+            iter_am = 1
+
+        # ruch hitboxa
+        # kwadrat.move_towards(player_pos)
 
         # zwiększenie wartości iteratora za każdym razem, gdy trwa atak mieczem (pozwalające kontynuować animację)
         if hitting:
@@ -161,6 +367,12 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
             iter = 0
             hitting = False
 
+        if movement_up or movement_left or movement_down or movement_right:
+            iter_pm += 1
+
+        if iter_pm >= len(player_move):
+            iter_pm = 1
+
         # animacja
         sword = sword_sprite[iter]
 
@@ -168,9 +380,35 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
         sword = pygame.transform.scale(sword, (sword_x_size, sword_y_size))
 
         # wyświetlenie miecza, kamienia i robaka
-        screen.blit(sword, (player_pos.x + stone_x_size, player_pos.y - stone_y_size / 2))
-        screen.blit(stone, (player_pos.x, player_pos.y))
-        screen.blit(bug, (bug_pos.x, bug_pos.y))
+        if left and last_left:
+            sword = pygame.transform.flip(sword, True, False)
+            screen.blit(sword, (player_pos.x - sword_x_size + 30, player_pos.y - stone_y_size / 2))
+        elif right and last_right:
+            screen.blit(sword, (player_pos.x + stone_x_size, player_pos.y - stone_y_size / 2))
+        elif last_right:
+            screen.blit(sword, (player_pos.x + stone_x_size, player_pos.y - stone_y_size / 2))
+        elif last_left:
+            sword = pygame.transform.flip(sword, True, False)
+            screen.blit(sword, (player_pos.x - sword_x_size + 30, player_pos.y - stone_y_size / 2))
+        else:
+            screen.blit(sword, (player_pos.x + stone_x_size, player_pos.y - stone_y_size / 2))
+
+        if left:
+            stone = pygame.transform.flip(stone, True, False)
+            screen.blit(stone, (player_pos.x, player_pos.y))
+        else:
+            screen.blit(stone, (player_pos.x, player_pos.y))
+
+        if bug_right:
+            bug = pygame.transform.flip(bug, True, False)
+            screen.blit(bug, (bug_pos.x, bug_pos.y))
+        else:
+            screen.blit(bug, (bug_pos.x, bug_pos.y))
+
+        pygame.draw.rect(screen, (0, 128, 0), rect_stone_hp)
+        pygame.draw.rect(screen, (255, 0, 0, 0), rect_bug_hp)
+
+        # kwadrat.draw_1(bug)
 
         # kolizja obiektów
         # - gdy zderzy się miecz i przeciwnik oraz następuje atak mieczem, zdrowie przeciwnika spada o 10
@@ -190,8 +428,8 @@ def start_game(): #funkcja po naciśnięcia przycisku "start"
         elif stone_hp <= 0:
             print("Game over")
             program_running = False
-
-        """poniżej zakomentowane metody pozwalające pokolorować hitboxy poszczególnych obiektów, w celu sprawdzenia sposobu ich zachowania się
+        """
+        #poniżej zakomentowane metody pozwalające pokolorować hitboxy poszczególnych obiektów, w celu sprawdzenia sposobu ich zachowania się
 
         pygame.draw.rect(screen, (0, 0, 0), rect_stone)
         pygame.draw.rect(screen, (0, 0, 255), rect_sword)
