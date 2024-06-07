fa84a6112e38c64ca1e597bdbc7614b6b3a09aba
kawiater <164020734+kawiater@users.noreply.github.com>
2024-06-07 15:54:41 +0200

    Update main.py - dodanie menu pauzy

--- main.py
+++ main.py
@@ -15,6 +15,34 @@ bug_resp = False
 bug_resp_2 = False
 bug_resp_3 = False
 
+def pause_menu():
+    pygame.draw.rect(surface, (127, 127, 127, 150), [0,0, WIDTH, HEIGHT])
+    pygame.draw.rect(surface, 'gray' , [200,150, 600, 50],0, 10)
+    resume = pygame.draw.rect(surface, 'black' , [200,220, 280, 50],0, 10)
+    exit= pygame.draw.rect(surface, 'black' , [520,220, 280, 50],0, 10)
+    surface.blit(font.render('Pause', True, 'white'), (220,160))
+    surface.blit(font.render('Resume', True, 'white'), (220,230))
+    surface.blit(font.render('Exit', True, 'white'), (540,230))
+    screen.blit(surface, (0,0))
+ pygame.display.update()
+    return resume, exit
+while true:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                paused = not paused
+
+        if false:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if resume.collidepoint(mouse_pos):
+                    paused = False
+                elif exit.collidepoint(mouse_pos):
+                    pygame.quit()
+                    sys.exit()
+
 class Wrog:
     def __init__(self, kolor_hitboxu, nazwa_wroga, wspolrzedna_x, wspolrzedna_y, szerokosc_wroga, wysokosc_wroga, grafika):
         self.nazwa = nazwa_wroga
