5ca90e7d4c889604c8baa2f954d96f97dd091722
kawiater <164020734+kawiater@users.noreply.github.com>
2024-06-09 01:02:05 +0200

    Update pieniazek.py

--- pieniazek.py
+++ pieniazek.py
@@ -4,3 +4,5 @@ class Coin:
         self.image = pygame.image.load('coin.png')  # Ścieżka do obrazu monety
         self.rect = self.image.get_rect(center=(x, y))
         self.collected = False
+        self.rect.x = random.randint(0, screen_width - self.rect.width)
+        self.rect.y = random.randint(0, screen_height - self.rect.height)
